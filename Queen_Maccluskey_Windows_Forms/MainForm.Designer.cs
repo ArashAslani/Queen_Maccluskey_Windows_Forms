4a439b2e5df736b9d09c2123a59c25b5cf2c8140
Arash Aslani <arashaslani2002@gmail.com>
2023-05-10 23:00:52 +0430

    Set name and text of tools.

--- Queen_Maccluskey_Windows_Forms/MainForm.Designer.cs
+++ Queen_Maccluskey_Windows_Forms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Queen_Maccluskey_Windows_Forms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.variableNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.submitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.variableNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -53,74 +53,75 @@
             this.textBox3.Size = new System.Drawing.Size(656, 23);
             this.textBox3.TabIndex = 2;
             // 
-            // numericUpDown1
+            // variableNumericUpDown
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(121, 21);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
-            this.numericUpDown1.TabIndex = 3;
+            this.variableNumericUpDown.Location = new System.Drawing.Point(121, 21);
+            this.variableNumericUpDown.Name = "variableNumericUpDown";
+            this.variableNumericUpDown.Size = new System.Drawing.Size(120, 23);
+            this.variableNumericUpDown.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Location = new System.Drawing.Point(8, 82);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(100, 15);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "Don\'t care terms :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Location = new System.Drawing.Point(8, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.Size = new System.Drawing.Size(64, 15);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "Minterms :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 50);
+            this.label3.Location = new System.Drawing.Point(8, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(107, 15);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "Number of values :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(332, 21);
+            this.label4.Location = new System.Drawing.Point(195, 3);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.Size = new System.Drawing.Size(454, 15);
             this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
+            this.label4.Text = "This program simplifies your logical expressions using Quinn McCluskey\'s algorith" +
+    "m.";
             // 
-            // button1
+            // submitBtn
             // 
-            this.button1.Location = new System.Drawing.Point(658, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.submitBtn.Location = new System.Drawing.Point(658, 21);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(119, 23);
+            this.submitBtn.TabIndex = 8;
+            this.submitBtn.Text = "Make it simple";
+            this.submitBtn.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.submitBtn);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.variableNumericUpDown);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.variableNumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,11 +131,11 @@
 
         private TextBox textBox1;
         private TextBox textBox3;
-        private NumericUpDown numericUpDown1;
+        private NumericUpDown variableNumericUpDown;
         private Label label1;
         private Label label2;
         private Label label3;
         private Label label4;
-        private Button button1;
+        private Button submitBtn;
     }
 }
--- Queen_Maccluskey_Windows_Forms/MainForm.cs
+++ Queen_Maccluskey_Windows_Forms/MainForm.cs
@@ -1,8 +1,8 @@
 namespace Queen_Maccluskey_Windows_Forms
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
