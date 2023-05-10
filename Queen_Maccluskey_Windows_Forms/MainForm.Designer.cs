0bf31ad436261218fce9958a62a0dea35aa96daf
Arash Aslani <arashaslani2002@gmail.com>
2023-05-10 23:12:43 +0430

    Result box and information added

--- Queen_Maccluskey_Windows_Forms/MainForm.Designer.cs
+++ Queen_Maccluskey_Windows_Forms/MainForm.Designer.cs
@@ -36,26 +36,32 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.submitBtn = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.variableNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(121, 50);
+            this.textBox1.Location = new System.Drawing.Point(121, 63);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(656, 23);
             this.textBox1.TabIndex = 0;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(121, 79);
+            this.textBox3.Location = new System.Drawing.Point(121, 92);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(656, 23);
             this.textBox3.TabIndex = 2;
             // 
             // variableNumericUpDown
             // 
-            this.variableNumericUpDown.Location = new System.Drawing.Point(121, 21);
+            this.variableNumericUpDown.Location = new System.Drawing.Point(121, 34);
             this.variableNumericUpDown.Name = "variableNumericUpDown";
             this.variableNumericUpDown.Size = new System.Drawing.Size(120, 23);
             this.variableNumericUpDown.TabIndex = 3;
@@ -63,7 +69,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 82);
+            this.label1.Location = new System.Drawing.Point(8, 95);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 15);
             this.label1.TabIndex = 4;
@@ -72,7 +78,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 53);
+            this.label2.Location = new System.Drawing.Point(8, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 15);
             this.label2.TabIndex = 5;
@@ -81,7 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 23);
+            this.label3.Location = new System.Drawing.Point(8, 36);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(107, 15);
             this.label3.TabIndex = 6;
@@ -90,7 +96,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(195, 3);
+            this.label4.Location = new System.Drawing.Point(195, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(454, 15);
             this.label4.TabIndex = 7;
@@ -99,18 +105,79 @@
             // 
             // submitBtn
             // 
-            this.submitBtn.Location = new System.Drawing.Point(658, 21);
+            this.submitBtn.Location = new System.Drawing.Point(658, 34);
             this.submitBtn.Name = "submitBtn";
             this.submitBtn.Size = new System.Drawing.Size(119, 23);
             this.submitBtn.TabIndex = 8;
             this.submitBtn.Text = "Make it simple";
             this.submitBtn.UseVisualStyleBackColor = true;
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(97, 488);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(276, 15);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.linkedin.com/in/arash-aslani-2002-ge";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Result : ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(121, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(656, 345);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 488);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Developed by\r";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(404, 488);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Github ";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(518, 490);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(60, 15);
+            this.linkLabel2.TabIndex = 14;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://github.com/ArashAslani/Queen_Maccluskey_Windows_Forms";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 512);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.submitBtn);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -137,5 +204,11 @@
         private Label label3;
         private Label label4;
         private Button submitBtn;
+        private LinkLabel linkLabel1;
+        private Label label5;
+        private RichTextBox richTextBox1;
+        private Label label6;
+        private Label label7;
+        private LinkLabel linkLabel2;
     }
 }
